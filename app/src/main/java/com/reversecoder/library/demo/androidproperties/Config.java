a08cdb166a73d9f01799617a90471c85cd1b0232
Md. Rashadul Alam <rashedul.alam@bjitgroup.com>
2017-03-27 14:09:07 +0600

    rc-library: read properties file from assets

---
+++ app/src/main/java/com/reversecoder/library/demo/androidproperties/Config.java
@@ -0,0 +1,19 @@
+package com.reversecoder.library.demo.androidproperties;
+
+import android.content.Context;
+
+import com.reversecoder.library.androidproperties.AssetsProperties;
+import com.reversecoder.library.androidproperties.Property;
+
+public class Config extends AssetsProperties {
+
+  @Property int max;
+  @Property("rate_value") float rateValue;
+  @Property double temperature;
+  @Property String message;
+  @Property boolean condition;
+
+  public Config(Context context) {
+    super(context);
+  }
+}
